8e3291546cf77b17012b82efd442a1255964f0e7
DusekDan <dusekdan@gmail.com>
2019-03-31 21:54:19 +0200

    Add - Skeleton of SiteCopier (first real module)

---
+++ modules/SiteCopier/SiteCopier.py
@@ -0,0 +1,10 @@
+class SiteCopier():
+
+    def __init__(self):
+        self.dependencies = []
+    
+    def execute(self):
+        print("SiteCopier: Executing...")
+
+    def get_dependencies(self):
+        return self.dependencies
