8c67ca1fd70b7412747fa22f603d6e6aad94d78f
DusekDan <dusekdan@gmail.com>
2019-03-31 21:51:44 +0200

    Update - Debugging logic for imported/discovered modules

--- debugger.py
+++ debugger.py
@@ -5,11 +5,18 @@ class DEBUG():
     def __init__(self, DEBUG_ENABLED = False):
         self.enabled = DEBUG_ENABLED
 
-    def discovered_modules(self, discovered_modules):
+    def discovered_modules_DEPRECATED(self, discovered_modules):
         if (self.enabled):
             print(" Discovered modules:")
             for module in discovered_modules:
                 print("\t|-> %s" % module)
+    
+    def discovered_modules(self):
+        all_modules = sys.modules.keys()
+        print(" Discovered modules:")
+        for module_name in all_modules:
+            if module_name.startswith('modules.') and module_name.count('.') == 1:
+                print("\t|-> %s" % module_name)
 
     def importing_module(self, module_name):
         if (self.enabled):
