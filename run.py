f533c9b7640d340fafd740cbfabd742293b95a0a
DusekDan <dusekdan@gmail.com>
2019-03-31 21:54:48 +0200

    Refactor - run.py now uses module_loader to load modules

--- run.py
+++ run.py
@@ -1,37 +1,36 @@
 import os
 import sys
 import importlib
+from core import utils 
+from core.module_loader import ModuleLoader
+
 from debugger import DEBUG
-#from SiteMapper import SiteMapper
-#from modules.Requester import Requester
 
 modules_folder = "modules"
 DBG = DEBUG(DEBUG_ENABLED=True)
 
-# 1 - Obtain existing modules from modules folder.
-discovered_modules = [m for m in os.listdir(modules_folder) if os.path.isdir(modules_folder + "/" + m)]
-DBG.discovered_modules(discovered_modules)
+ML = ModuleLoader(modules_folder)
+instantiated_modules = ML.discover_modules()
 
-# 2 - Import them using the importlib.import_module 
-# |-> see https://stackoverflow.com/a/301146/3444151
-# |-> see https://stackoverflow.com/a/4821120/3444151
-instantiated_modules = {}
-for module in discovered_modules:
-    module_path = modules_folder + "." + module + "." + module
-    DBG.importing_module(module_path)
-    
-    imported_module = importlib.import_module(module_path)
+# DBG.imported_modules()
+DBG.discovered_modules()
 
-    instantiable = getattr(imported_module, module)
-    instantiated_modules[module] = instantiable()
+# 3 - Run them via standardized calls (Is this called the API?) 
+# # TODO: Make this follow process flow described in module.loading.strategy.md
+for module_name, instance in instantiated_modules.items():
+    DBG.starting_module(module_name)
+    instance.execute()
 
-DBG.imported_modules()
+
 
 # TODO: X - Optional: Verify loaded modules are in standardized shape (have proper methods etc.)
 # If this todo is not worked in, loading will start to fail once the improperly written modules
 # start to appear. Huge except-catch potential here.
 
-# 3 - Run them via standardized calls (Is this called the API?)
-for module_name, instance in instantiated_modules.items():
-    DBG.starting_module(module_name)
-    instance.execute()
+
+
+# Old code fragmets. To be deleted, due 10th April, 2019
+
+# 1 - Obtain existing modules from modules folder.
+#discovered_modules = [m for m in os.listdir(modules_folder) if os.path.isdir(modules_folder + "/" + m)]
+#DBG.discovered_modules(discovered_modules)
