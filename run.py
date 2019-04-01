ebeb2fbbca43c89fe205a0e5ae3c5c395afb558e
DusekDan <dusekdan@gmail.com>
2019-04-01 20:44:41 +0200

    Update - Progress on independent and dependent module execution

--- run.py
+++ run.py
@@ -17,8 +17,62 @@ instantiated_modules = ML.discover_modules()
 DBG.discovered_modules()
 
 # 2 - Classify modules by its ability to be run
+
 independent, satisfiable, nonrunnable = ML.classify_modules(instantiated_modules)
 DBG.classified_modules(independent, satisfiable, nonrunnable)
+
+# 3 - Run independent modules and store run outputs
+
+module_results = {}
+
+for module_name, instance in independent.items():
+    
+    exit_flag = instance.execute("https://danieldusek.com")
+    results = instance.get_results()
+    physical_artifacts = instance.leaves_phisical_artifacts()
+    
+    module_results[module_name] = {
+        "exit_flag": exit_flag,
+        "results": results,
+        "left_physical_artifacts": physical_artifacts
+    }
+
+    print("[I] Module %s finished and saved results." % module_name)
+
+print("[I] Independent run finished.")
+
+# 4 - Check whether it is possible to run satisfiably dependent modules
+
+try_again = False
+# TODO: This needs to be wrapped in while loop until try again == False (update the condition)
+for module_name, instance in satisfiable.items():
+    
+    required_dependencies = instance.get_dependencies()
+    
+    can_run = True
+    for dependency in required_dependencies:
+        if dependency["depends_on"] not in module_results.keys():
+            can_run = False
+            print("[W] Some modules can not be run. Dependency is not present in module_results.")
+            print("\t %s -> %s" % (module_name, dependency["depends_on"]))
+            break
+    
+    if can_run:
+        exit_flag = instance.execute("https://danieldusek.com")
+        results = instance.get_results()
+        physical_artifacts = instance.leaves_phisical_artifacts()
+
+        module_results[module_name] = {
+            "exit_flag": exit_flag,
+            "results": results,
+            "left_physical_artifacts": physical_artifacts
+        }
+
+        try_again = True
+    
+    if try_again == False:
+        print("[W] Some modules can not be run. Maybe there is a circular dependency?")
+
 
 # 3 - Run them via standardized calls (Is this called the API?) 
 # # TODO: Make this follow process flow described in module.loading.strategy.md
