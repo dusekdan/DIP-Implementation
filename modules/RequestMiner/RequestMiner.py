33e9648cfdc40c8541d85d50f1d3238977775265
DusekDan <dusekdan@gmail.com>
2019-04-17 11:47:33 +0200

    Refactor - Polished existing param finding + removed internal urlhelper

--- modules/RequestMiner/RequestMiner.py
+++ modules/RequestMiner/RequestMiner.py
@@ -3,9 +3,9 @@ import random
 import requests
 import core.utils as utils
 import core.config as cfg
+from core.helpers import URLHelper
 
 from urllib.parse import urlparse, urljoin, parse_qs, parse_qsl
-from requests.models import PreparedRequest
 
 class RequestMiner():
 
@@ -20,25 +20,18 @@ class RequestMiner():
         self.module_name = "RequestMiner"
         self.sitecopier_results = {}
 
+        self.URLHelper = URLHelper()
+
         self.CANARY_LENGTH = 8
         self.MAX_REFLECTION_REQUESTS = 10
 
-        # structure[parameterName] = [baseUrl1, baseUrl2]
-        # Proposed updated structure:
-        # structure[parameterName] = {
-        #   baseUrl1: originalUrlWithValues,
-        #   baseUrl2: originalUrlWithValues
-        # } -> this should help with discovering reflection
-        # in these parameters.
-        self.discovered_q_pars = {}
-
         # struct[param] = {
-        #   "sources": [ 'url1', 'url2', 'url3' ],
+        #   "sources": ['url1', 'url2', 'url3'],
         #   "values": ['value1', 'value2', 'value3'],
         #   "reflects": True|False|None
+        #   "reflects_on": ['url1', 'url2']
         # }
-        self.newdiscovered_qpars = {}
-        
+        self.discovered_params = {}
         self.discovered_headers = []
     
     
@@ -50,61 +43,55 @@ class RequestMiner():
         self.mprint("===================================%s===================================" % self.module_name)
         self.target = param
 
-        # TODO: Discover Headers + Filter standard (uninteresting) response headers
-        self.mprint("Seaching for URL parameters...")
+        self.mprint("Seaching for used query string parameters & headers...")
         source = os.path.join("output", cfg.CURRENT_RUN_ID, "SiteCopier")
         for id in range(len(os.listdir(source))):
             url = self.obtain_id_url(id)
-            #TODO: uncomment this - commented out only for testing on dd.com
-            #if not self.is_in_scope(self.target, url):
+            # TODO: uncomment this - commented out only for testing on dd.com
+            #if not self.URLHelper.is_in_scope(self.target, url):
             #    continue
             response = os.path.join(source, str(id), "%s.response" % id)
             headers = "%s.headers" % response
-            
-            # TODO: Comment out following 4 lines of code then rewrite add_discovered_params to contain logic after "New way of adding params"
-            params = parse_qs(urlparse(url).query, keep_blank_values=True).keys()
+
+            # Existing Parameter Discovery
+            params = parse_qs(urlparse(url).query, keep_blank_values=True)
             self.mprint(
             "Discovered %s new parameters." % self.add_discovered_params(url, params)
             )
 
-            # New way of recording params
-            params = parse_qs(urlparse(url).query, keep_blank_values=True)
-            for param, values in params.items():
-                # Record for given parameter does not exist
-                if param not in self.newdiscovered_qpars.keys():
-                    self.newdiscovered_qpars[param] = {
-                        "sources": [url],
-                        "values": values,
-                        "reflects": None
-                    }
-                else:
-                    # Source not recorded yet
-                    if url not in self.newdiscovered_qpars[param]["sources"]:
-                        self.newdiscovered_qpars[param]["sources"].append(url)
-                    # Values not recorded yet
-                    self.newdiscovered_qpars[param]["values"] += values
-                    # Following construction erases any assumptions about list order.
-                    self.newdiscovered_qpars[param]["values"] = list(set(
-                        self.newdiscovered_qpars[param]["values"]
-                    ))
-
-            self.mprint(self.newdiscovered_qpars)
-
+            # Existing Header Discovery
             discovered_headers = self.filter_common_headers(
-                self.discover_headers(headers))
+            self.discover_headers(headers))
             self.mprint(
             "Discovered %s new headers." % self.add_discovered_headers(discovered_headers)
             )
+
+
+
+        # ! Find hidden URL parameters
+        # TODO: Fuzz discover URL params (probably on the most URL-param-rich sites?)
+        # * IMPLEMENTATION OUTLINE
+        # * - Determine what heuristic to use for 'mining' URL selection
+        # * - Determine how to properly mine it (how to verify response, how to react to
+        # * specific responses, how to 'pre-flight' it first)
+        # * - How to record it: Separate hidden params structure vs Shared structure
+        # * - 
+        
+        # ! Find hidden Headers
+        # TODO: Fuzz discover headers
+
+
+
         
         # Go through the existing URL parameters and figure out whether some of
         # them are reflected into the response.
-        for param_name, param_record in self.newdiscovered_qpars.items():
+        for param_name, param_record in self.discovered_params.items():
             reflections = self.test_parameter_reflection(param_name, param_record)
             if reflections:
                 # Update discovered qpars structure with reflects: True and reflects on.
                 self.mprint("Parameter %s reflects!" % param_name)
-                self.newdiscovered_qpars[param_name]["reflects"] = True
-                self.newdiscovered_qpars[param_name]["reflects_on"] = reflections
+                self.discovered_params[param_name]["reflects"] = True
+                self.discovered_params[param_name]["reflects_on"] = reflections
             else:
                 self.mprint("Nope, Param %s does not reflect." % param_name)
 
@@ -112,12 +99,10 @@ class RequestMiner():
         # FUTURE: Evaluate security standing for discovered headers (will require
         # keeping values of the headers (not currently doing that... aaah))
         # Maybe this functionality will fit better under SSL eval module?
-
+        
         # TODO: Check reflections on URL/Headers
-        # TODO: Fuzz discover URL params (probably on the most URL-param-rich sites?)
-        # TODO: Fuzz discover Header params
-
-        self.mprint("Terminating...")
+        
+        self.mprint("Request mining completed....")
         self.mprint("===================================%s===================================" % self.module_name)
 
 
@@ -138,7 +123,7 @@ class RequestMiner():
         
         for url_class, values in url_classes.items():
             # Pick exactly 1 candidate from each group
-            current_target = self.replace_parameter_value(values[0],
+            current_target = self.URLHelper.replace_parameter_value(values[0],
                 parameter_name, canary)
 
             if reflection_requests <= self.MAX_REFLECTION_REQUESTS:
@@ -154,21 +139,6 @@ class RequestMiner():
                 break
         
         return reflects_on
-
-
-    def replace_parameter_value(self, url, parameter_name, value):
-        """
-        Replaces value of the parameter in query string with desired value.
-        TODO: Move this into URL helper it's located under utils.
-        """
-        parts = urlparse(url)
-        query_dict = dict(parse_qsl(parts.query))
-        query_dict[parameter_name] = value
-        req = PreparedRequest()
-        req.prepare_url(
-            parts.scheme + "://" + parts.netloc + parts.path, query_dict
-        )
-        return req.url
 
 
     def classify_param_sources(self, sources):
@@ -190,26 +160,33 @@ class RequestMiner():
         return classes
 
 
-    # TODO: This might be reworked to store the 'newd' structure soon
     def add_discovered_params(self, url, params):
         """
-        Adds param into the discovered params list if it was not already there.
-        TODO: Store the whole URL
+        Adds newly discovered parameter into the list (if it was not there).
+        Returns a number of newly discovered parameters.
         """
         discovered = 0
-        parts = urlparse(url)
-        base_url = parts.scheme + "://" + parts.netloc + parts.path
-        for param in params:
-            # Param already discovered
-            if param in self.discovered_q_pars.keys():
-                # But origin URL not registered
-                if base_url not in self.discovered_q_pars[param]:
-                    self.discovered_q_pars[param].append(base_url)
-            # Param is new
-            else:
-                discovered += 1
-                self.mprint("%s on %s (%s)" % (param, base_url, url))
-                self.discovered_q_pars[param] = [base_url]
+        for param, values in params.items():
+                if param not in self.discovered_params.keys():
+                    # Record for given parameter does not exist
+                    self.discovered_params[param] = {
+                        "sources": [url],
+                        "values": values,
+                        "reflects": None
+                    }
+                    discovered += 1
+                else:
+                    # Source not recorded yet
+                    if url not in self.discovered_params[param]["sources"]:
+                        self.discovered_params[param]["sources"].append(url)
+                    
+                    # Update discovered values - Note that this erases any 
+                    # assumptions about the order of values.
+                    self.discovered_params[param]["values"] += values
+                    self.discovered_params[param]["values"] = list(
+                        set(self.discovered_params[param]["values"])
+                    )
+        
         return discovered
 
 
@@ -284,25 +261,4 @@ class RequestMiner():
 
 
     def leaves_physical_artifacts(self):
-        return False
-
-
-    def is_in_scope(self, scope, url):
-        """
-        Decides whether given URL is within the scope of the target 
-        application.
-
-        Subdomain 'www.' is automatically considered to be in-scope.
-
-        REFACTOR: Once URLHelper is in utils, use it from there (2/3 copy rule)
-        """
-        url_parts = urlparse(url)
-        scope_parts = urlparse(scope)
-
-        if url_parts.netloc == scope_parts.netloc:
-            return True
-        
-        if url_parts.netloc == 'www.' + scope_parts.netloc:
-            return True
-        
         return False
