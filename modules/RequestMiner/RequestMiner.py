e3b71b8a360b43960679b36dd403199c6e5655b9
DusekDan <dusekdan@gmail.com>
2019-04-17 22:29:14 +0200

    Update - Add advanced query string discovery functionality

--- modules/RequestMiner/RequestMiner.py
+++ modules/RequestMiner/RequestMiner.py
@@ -4,8 +4,12 @@ import requests
 import core.utils as utils
 import core.config as cfg
 from core.helpers import URLHelper
+from requests.models import PreparedRequest
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 from urllib.parse import urlparse, urljoin, parse_qs, parse_qsl
+from time import sleep
 
 class RequestMiner():
 
@@ -22,8 +26,12 @@ class RequestMiner():
 
         self.URLHelper = URLHelper()
 
+        self.DELAY = 0.1
         self.CANARY_LENGTH = 8
         self.MAX_REFLECTION_REQUESTS = 10
+
+        self.URLPARAM_DISCOVERY_HEURISTICS = "START_PAGE"
+        self.MAX_ACCEPTED_URL_LENGTH = 2000 # Sourced: https://stackoverflow.com/a/417184/
 
         # struct[param] = {
         #   "sources": ['url1', 'url2', 'url3'],
@@ -33,6 +41,8 @@ class RequestMiner():
         # }
         self.discovered_params = {}
         self.discovered_headers = []
+
+        self.url_discovery_parameters = []
     
     
     def mprint(self, string):
@@ -67,22 +77,19 @@ class RequestMiner():
             )
 
 
+        # Find hidden URL parameters
+        pd_target = self.urlparam_startpage_heuristics()
+        self.mprint(
+            "Initializing parameter mining: %s" % pd_target
+        )
+        discovered_ps, reflected_ps = self.discover_hidden_url_parameters(pd_target)
+        self.mprint("Discovered params: %s | Reflecting params: %s" % (len(discovered_ps, reflected_ps)))
 
-        # ! Find hidden URL parameters
-        # TODO: Fuzz discover URL params (probably on the most URL-param-rich sites?)
-        # * IMPLEMENTATION OUTLINE
-        # * - Determine what heuristic to use for 'mining' URL selection
-        # * - Determine how to properly mine it (how to verify response, how to react to
-        # * specific responses, how to 'pre-flight' it first)
-        # * - How to record it: Separate hidden params structure vs Shared structure
-        # * - 
-        
+
         # ! Find hidden Headers
         # TODO: Fuzz discover headers
 
 
-
-        
         # Go through the existing URL parameters and figure out whether some of
         # them are reflected into the response.
         for param_name, param_record in self.discovered_params.items():
@@ -100,10 +107,292 @@ class RequestMiner():
         # keeping values of the headers (not currently doing that... aaah))
         # Maybe this functionality will fit better under SSL eval module?
         
-        # TODO: Check reflections on URL/Headers
+        # TODO: Check reflections Headers
         
         self.mprint("Request mining completed....")
         self.mprint("===================================%s===================================" % self.module_name)
+
+
+    def discover_hidden_url_parameters(self, target):
+        """
+        Rains down the furious requeststorm upon thy target to figure out what
+        parameters it had enshrouded.
+        """
+        discovery_urls, canaries = self.prepare_param_discovery_urls(target)
+        discovered_params = []
+        reflected_params = []
+        
+        # Preliminary analysis of target response to certain requests.
+        ok_response = {}
+        non_existing_q_param = utils.get_rnd_string(15)
+        non_existing_q_param_value = utils.get_rnd_string(10)
+        param_not_exists_response = {}
+        try:
+            std_r = self._retry_session().get(target)
+            ok_response["code"] = std_r.status_code
+            ok_response["text"] = std_r.text
+            ok_response["headers"] = std_r.headers
+
+            pne_r = self._retry_session().get(
+                self.URLHelper.add_query_string_param(
+                    target, non_existing_q_param, non_existing_q_param_value
+                ))
+            param_not_exists_response["code"] = pne_r.status_code
+            param_not_exists_response["text"] = pne_r.text
+            param_not_exists_response["headers"] = pne_r.headers
+
+            # Randomly generated query string param was discovered in the response,
+            # there is a strong chance that every string param is reflected.
+            if (non_existing_q_param in param_not_exists_response["text"] 
+                or non_existing_q_param_value in param_not_exists_response["text"]):
+                discovered_params.append(non_existing_q_param)
+                reflected_params.append(non_existing_q_param)
+        
+        except: # ! WHAT THE HELL
+            return []
+
+        hidden_parameters = self.mine_hidden_parameters(discovery_urls, 
+            canaries, ok_response, param_not_exists_response
+        )
+
+        discovered_params += hidden_parameters["discovered"]
+        reflected_params += hidden_parameters["reflected"]
+        
+        return (discovered_params, reflected_params)
+
+
+    def mine_hidden_parameters(self, urls, canaries, ref_ok, ref_pne):
+        """
+        Sends requests to prepared discovery URLs and returns lists of 
+        discovered parameters & list of reflective parameters.
+
+        FUTURE: Calculate the difference between .text responses and reflect it in the indicator. 
+        TODO: Think more about how to stop parameter mining when everything is reflecting.
+        """
+        reflected_params = []
+        discovered_params = []
+        run_through = 0
+        for url in urls:
+            try:
+                r = self._retry_session().get(url)
+                self.mprint("Requesting: %s" % url)
+                # Indicators of whether parameter has any effect
+                # [code, headers, text], forEach x, x € [0, 0.5, 1]
+                indicators = [
+                self.rate_indicators(
+                    r.status_code, 
+                    ref_ok["code"], 
+                    ref_pne["code"]
+                ),
+                self.rate_indicators(
+                    len(r.headers), 
+                    len(ref_ok["headers"]), 
+                    len(ref_pne["headers"])
+                ),
+                self.rate_indicators(
+                    len(r.text), len(ref_ok["text"]), len(ref_pne["text"]))
+                ]
+
+                print("Indicators: %s" % indicators)
+
+                # Look for canaries & responsible parameters
+                if sum(indicators) > 0:
+                    self.mprint("Found something... will try to pinpoint the parameter.")
+                    effective_params = self.identify_parameter2(url, ref_ok, ref_pne)
+                    if effective_params:
+                        discovered_params += effective_params
+
+                    for name, value in canaries.items():
+                        if value in r.text:
+                            self.mprint("%s was in the text. Adding %s" % (value, name))
+                            reflected_params.append(name)
+
+                    self.mprint("Discovered: %s" % discovered_params)
+                    self.mprint("Reflections: %s" % reflected_params)
+                
+                sleep(self.DELAY)
+            except: # ! WHAT THE HELL
+                pass
+            
+            if run_through == 0 and len(discovered_params) > 10:
+                self.mprint("INFO: Everything is probably reflecting. Stopping mining operations.")
+                break
+            run_through += 1
+
+        return {"reflected": reflected_params, "discovered": discovered_params}
+
+
+    def identify_parameter2(self, url, ok, notok):
+        """
+        Identifies parameters in URL that are responsible for detected changes
+        in the way target application replies. Request heavy and calls it off
+        when more than half of the batch of parameters influence responses.
+        """
+        added_params = list(
+            set(
+                list(parse_qs(urlparse(url).query).keys())
+            ) & set(self.url_discovery_parameters)
+        )
+        effective_params = []
+
+        for _ in range(len(added_params)):
+            to_append = random.choice(added_params)
+            self.mprint("Trying the %s parameter." % to_append)
+            appended = self.URLHelper.add_query_string_param(
+                self.urlparam_startpage_heuristics(), 
+                to_append, utils.get_rnd_string()
+            )
+            try:
+                r = self._retry_session().get(appended)
+                
+                ci = self.rate_indicators(
+                    r.status_code, ok["code"], notok["code"]
+                )
+                hi = self.rate_indicators(
+                    len(r.headers), len(ok["headers"]), len(notok["headers"])
+                )
+                ti = self.rate_indicators(
+                    len(r.text), len(ok["text"]), len(notok["text"])
+                )
+
+                if sum([ci, hi, ti]) > 0:
+                    effective_params.append(to_append)
+                
+                sleep(self.DELAY)
+            
+            except: #! What the hell
+                return None
+
+        return effective_params
+
+
+    def identify_parameter(self, url, ok, notok):
+        """
+        Identifies which of the parameters in URL is responsible for detected
+        change in the way target application replied. Kinda request heavy.
+        FUTURE: Figure how to do this efficiently with interval splitting.
+        TODO: Make it work / Use strip_tags()-like logic to compare plaintext.
+        """
+        # Intersection of params in URL and params from our parameter list.
+        added_params = list(
+            set(
+                list(parse_qs(urlparse(url).query).keys())
+            ) & set(self.url_discovery_parameters)
+        )
+        to_remove = random.choice(added_params)
+
+        for _ in range(len(added_params)):
+            self.mprint("Trying the %s parameter." % to_remove)
+            removed = self.URLHelper.remove_query_string_param(url, to_remove)
+            try:
+                r = self._retry_session().get(removed)
+
+                ci = self.rate_indicators(
+                    r.status_code, ok["code"], notok["code"]
+                )
+                hi = self.rate_indicators(
+                    len(r.headers), len(ok["headers"]), len(notok["headers"])
+                )
+                ti = self.rate_indicators(
+                    len(r.text), len(ok["text"]), len(notok["text"])
+                )
+
+                # This means that the parameter to_remove made the change
+                # If it's >0, then the remaining parameters made the change
+                if sum([ci, hi, ti]) == 0:
+                    return to_remove
+                elif sum([ci, hi, ti]) > 0:
+                    url = removed
+
+                added_params.remove(to_remove)
+                if len(added_params) != 0:
+                    to_remove = random.choice(added_params)
+
+
+                sleep(self.DELAY)
+
+            except: #! What the hell
+                return None
+
+
+
+    def rate_indicators(self, current, ok, pne):
+        """
+        Returns likelyhood with which current request contained parameter that
+        affected target's response.
+        """
+        if pne == ok:
+            if ok == current:
+                return 0
+            else:
+                return 1
+        else:
+            if current == pne:
+                return 0
+            elif current == ok:
+                return 1
+            else:
+                return 0.5
+
+
+    def prepare_param_discovery_urls(self, base_url):
+        """
+        For given URL generates bunch of parameter discovery URLs with 
+        canaries and returns both of them back.
+        """
+        PARAM_SEP_LEN = 2 ; CANARY_LEN = self.CANARY_LENGTH
+        url_len = len(base_url)
+        
+        discovery_urls = []
+        param_dict = {}
+        canary_dict = {}
+
+        try:
+            with open('payloads/parameters.txt', 'r') as f:
+                param_list = f.read().splitlines()
+                self.url_discovery_parameters = param_list
+        except IOError:
+            self.mprint("[ERROR] Unable to open payloads/parameters.txt.")
+            return
+        
+        for param in param_list:
+            if (url_len + len(param) + PARAM_SEP_LEN + CANARY_LEN) < self.MAX_ACCEPTED_URL_LENGTH:
+                # URL Character limit not exceeded yet.
+                canary = utils.get_rnd_string(CANARY_LEN)
+                param_dict[param] = canary
+                canary_dict[param] = canary
+                url_len += (PARAM_SEP_LEN + len(param) + CANARY_LEN)
+            else:
+                # Add to URLs, start preparing a new one. 
+                r = PreparedRequest()
+                r.prepare_url(base_url, param_dict)
+                discovery_urls.append(r.url)
+                param_dict = {}
+                url_len = len(base_url)
+        
+        # Leftovers after depleting all parameters from paramlist.
+        if len(param_dict) != 0:
+            r = PreparedRequest()
+            r.prepare_url(base_url, param_dict)
+            discovery_urls.append(r.url)
+
+        return (discovery_urls, canary_dict)
+
+
+    def urlparam_startpage_heuristics(self):
+        """
+        Select which URL will be used as a base URL against which the query 
+        parameters discovery will be executed.
+        FUTURE: Selection heuristics cab be influenced by the set_options
+        Available heuristics:
+        - (A) Base URL (main page)
+        - (B) Page with the highest amount of query parameters
+        """
+        if self.URLPARAM_DISCOVERY_HEURISTICS == "START_PAGE":
+            return self.target
+        
+        if self.URLPARAM_DISCOVERY_HEURISTICS == "MOST_PARAMS":
+            return self.target # TODO: Figure out how to detect this page.
 
 
     def test_parameter_reflection(self, parameter_name, parameter_record):
@@ -262,3 +551,25 @@ class RequestMiner():
 
     def leaves_physical_artifacts(self):
         return False
+
+
+    def _retry_session(self, 
+        retries = 5, 
+        backoff_factor = 0.3, 
+        status_forcelist = (500,502,503,504),
+        session = None):
+        """
+        Gets a retry session with default parameters unless specifically
+        requested otherwise.
+        """
+        session = session or requests.Session()
+        retry = Retry(
+            total= retries, read = retries, connect = retries,
+            backoff_factor = backoff_factor, status_forcelist = status_forcelist
+        )
+
+        adapter = HTTPAdapter(max_retries = retries)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+        
+        return session
